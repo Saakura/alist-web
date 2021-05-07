498c1c6b9756dcb3a66208fe3e2d17cd9c276241
Saakura <63957897+Saakura@users.noreply.github.com>
2021-05-07 16:22:32 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
   //放置生成的静态资源 (js、css、img、fonts) 的目录
   assetsDir: 'static',
   //指定生成的 index.html 的输出路径
-  indexPath: 'index.html',
+  indexPath: '/index.html',
   //去除map
   productionSourceMap:false,
   devServer: {
